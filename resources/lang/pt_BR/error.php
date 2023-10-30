bfca35153727184bc1ce8b02e8bda756ee8c56ac
barbosalb96 <barbosalucaslbs96@gmail.com>
2023-10-30 08:33:52 -0300

    atualização dos teste e adicição dos langs

---
+++ resources/lang/pt_BR/error.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "route_not_found" => "Rota não encontrada",
+    "unauthenticated" => "Não autenticado",
+    "method_not_allowed" => "Método não permitido",
+    "method_http_allowed" => "Método HTTP não permitido para esta rota.",
+];
