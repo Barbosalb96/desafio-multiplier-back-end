1d96958dfaeb02c68ac6b502e64016e9b29b0128
Lucas Barbosa <barbosalucaslbs96@gmail.com>
2023-11-01 17:16:22 -0300

    ajuste docker

--- docker/init.sh
+++ docker/init.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-run composer install
+cd /var/www
+php-fpm
+
+composer install
 chown -R www-data:www-data /var/www/html/storage
 chown -R www-data:www-data /var/www/html/bootstrap/cache
 
-php-fpm
+
+php artisan cache:clear
+php artisan route:clear
+php artisan optimize:clear
+php artisan migrate
+php artisan db:seed
